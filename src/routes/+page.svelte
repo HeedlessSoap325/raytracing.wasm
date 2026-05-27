9f80f595779f501915ce5ababfc27414299eaa63
M. Fritz <mathisjulian123@gmail.com>
2026-05-27 11:03:00 +0200

    feat: add test setup to display rendered output in svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
-	import init, { hello } from "$rust/lib";
+	import init, { render } from "$rust/lib";
     import { onMount } from "svelte";
+
+	let canvas: HTMLCanvasElement | undefined = $state(undefined);
+
+	async function doRender() {
+		const ctx = canvas!.getContext("2d");
+		const width  = 512;
+		const height = 512;
+
+		canvas!.width  = width;
+		canvas!.height = height;
+
+		const pixelData = render(BigInt(width), BigInt(height));
+		console.log(pixelData)
+
+		const imageData = new ImageData(	
+			new Uint8ClampedArray<ArrayBuffer>(pixelData.buffer as ArrayBuffer, pixelData.byteOffset, pixelData.byteLength),
+			width,
+			height
+		);
+
+		ctx!.putImageData(imageData, 0, 0);
+	}
 
 	onMount(async () => {
 		await init();
-		console.log(hello());
+	
 	});
 </script>
+
+<canvas bind:this={canvas}></canvas>
+
+<button onclick={doRender}>Render!</button>
