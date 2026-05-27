4737fcd90f7eef7980108c5f6e85934321ebb36c
M. Fritz <mathisjulian123@gmail.com>
2026-05-27 09:09:59 +0200

    feat: introduce svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import init, { hello } from "$rust/lib";
+    import { onMount } from "svelte";
+
+	onMount(async () => {
+		await init();
+		console.log(hello());
+	});
+</script>
