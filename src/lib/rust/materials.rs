cc95478ef6ebb3a30c52d9d5447b2b7068751286
M. Fritz <mathisjulian123@gmail.com>
2026-06-05 17:50:34 +0200

    feat: create a hollow glass sphere

--- src/lib/rust/materials.rs
+++ src/lib/rust/materials.rs
@@ -1,3 +1,4 @@
+use crate::utils::rand_f64;
 use crate::vec::{Vec3, Color};
 use crate::ray::{Ray};
 use crate::hit::{HitRecord};
@@ -61,6 +62,13 @@ impl Dielectric {
 	pub fn new(refraction_index: f64) -> Self {
 		Self { refraction_index }
 	}
+
+	fn reflectance(cos: f64, refraction_index: f64) -> f64 {
+		// Use Schlick's approximation for reflectance.
+		let mut r0: f64 = (1.0 - refraction_index) / (1.0 + refraction_index);
+		r0 = r0 * r0;
+		r0 + (1.0 - r0) * (1.0 - cos).powf(5.0)
+	}
 }
 
 impl Material for Dielectric {
@@ -75,7 +83,7 @@ impl Material for Dielectric {
 		let cannot_refract: bool = ri * sin_theta > 1.0;
 		let direction: Vec3;
 
-		if cannot_refract {
+		if cannot_refract || (Dielectric::reflectance(cos_theta, ri) > rand_f64()) {
 			direction = Vec3::reflect(unit_dir, rec.normal);
 		} else {
 			direction = Vec3::refract(unit_dir, rec.normal, ri);
